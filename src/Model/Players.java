7b53973415c83e112ccfa012061456e4de7b3f50
adrianbueno <a.bueno2@sapalomera.cat>
2024-05-17 12:13:19 +0200

    Creat constructor Players

---
+++ src/Model/Players.java
@@ -0,0 +1,89 @@
+package Model;
+
+import java.sql.Date;
+public class Players {
+    private int jugadorId;
+    private String nom;
+    private String cognom;
+    private Date dataNaixement;
+    private String alcada;
+    private String pes;
+    private String posicio;
+    private int equipId;
+
+    public Players(int jugadorId, String nom, String cognom, Date dataNaixement, String alcada, String pes, String posicio, int equipId) {
+        this.jugadorId = jugadorId;
+        this.nom = nom;
+        this.cognom = cognom;
+        this.dataNaixement = dataNaixement;
+        this.alcada = alcada;
+        this.pes = pes;
+        this.posicio = posicio;
+        this.equipId = equipId;
+    }
+    // Getters y Setters
+    public int getJugadorId() {
+        return jugadorId;
+    }
+
+    public void setJugadorId(int jugadorId) {
+        this.jugadorId = jugadorId;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCognom() {
+        return cognom;
+    }
+
+    public void setCognom(String cognom) {
+        this.cognom = cognom;
+    }
+
+    public Date getDataNaixement() {
+        return dataNaixement;
+    }
+
+    public void setDataNaixement(Date dataNaixement) {
+        this.dataNaixement = dataNaixement;
+    }
+
+    public String getAlcada() {
+        return alcada;
+    }
+
+    public void setAlcada(String alcada) {
+        this.alcada = alcada;
+    }
+
+    public String getPes() {
+        return pes;
+    }
+
+    public void setPes(String pes) {
+        this.pes = pes;
+    }
+
+    public String getPosicio() {
+        return posicio;
+    }
+
+    public void setPosicio(String posicio) {
+        this.posicio = posicio;
+    }
+
+    public int getEquipId() {
+        return equipId;
+    }
+
+    public void setEquipId(int equipId) {
+        this.equipId = equipId;
+    }
+}
+
