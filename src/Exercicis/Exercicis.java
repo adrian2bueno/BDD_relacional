c81d5df61e52a62858efd44ca609b29b3b52c544
adrianbueno <a.bueno2@sapalomera.cat>
2024-05-24 12:57:15 +0200

    Fets exercici 1 i 2

--- src/Exercicis/Exercicis.java
+++ src/Exercicis/Exercicis.java
@@ -1,38 +1,45 @@
 package Exercicis;
 
+import Model.DAO.PlayerDAO;
+import Model.DAO.TeamsDAO;
 import Model.Jugador;
 
+import java.sql.Date;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class Exercicis {
+    static TeamsDAO equipDAO = new TeamsDAO();
+    static PlayerDAO playerDAO = new PlayerDAO();
+
+
     //1.- Llistar tots els jugadors d'un equip
 
-
-
-    public static void llistarJugadorsEquip(List<Jugador> jugadors){
+    public static void exercici1(String equipNom) throws Exception {
+        List<Jugador> jugadors = equipDAO.obtenirJugadors(equipNom);
         for (Jugador jugador : jugadors) {
-            System.out.println(jugador.getNom());
+            System.out.println(jugador.getNom() + " " + jugador.getCognom());
         }
     }
     //2.- Calcular la mitjana de punts, rebots, assistències, ... d'un jugador
-
-
-    //3.- Llistar tots els partits jugats per un equip amb el seu resultat.
-    public static void llistarPartitsIResultats(List<Set<Map.Entry<String,Integer>>> llista){
-        for (Set<Map.Entry<String,Integer>> set : llista) {
-            for (Map.Entry<String,Integer> entry : set) {
-                System.out.println(entry.getKey() + ": " + entry.getValue());
-            }
-            System.out.println();
+    public static void exercici2(String jugadorNom) throws Exception {
+        LinkedHashMap<String,Float> mitjanes = playerDAO.calcularMitjana(jugadorNom);
+        for (Map.Entry<String, Float> entry : mitjanes.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
         }
     }
+
+    //3.- Llistar tots els partits jugats per un equip amb el seu resultat.
+
 
     //4.- Inserir un nou jugador a un equip.
 
 
+
     //5.- Traspassar un judador a un altra equip
+
 
 
     //6.- Actualitzar les dades de jugadors o equips després d'un partit.
