9ba3dbc29b4021e46a431be209e67c2deb91acf2
adrianbueno <a.bueno2@sapalomera.cat>
2024-05-26 19:51:44 +0200

    Dades login correctes

--- src/Model/DatabaseConnection.java
+++ src/Model/DatabaseConnection.java
@@ -3,37 +3,24 @@ package Model;
 import java.sql.*;
 //todo Hacer que se conecte a la API NBA https://github.com/albaamatamoros/PracticaUF6-NBA/blob/master/src/Model/Model.java
 //todo: Canviar nombres
-//todo:
-//todo:
-//todo:
-public class DatabaseConnection {
-    //https://stats.nba.com/stats/synergyplaytypes?LeagueID=00&PerMode=PerGame&PlayType=Isolation&PlayerOrTeam=P&SeasonType=Playoffs&SeasonYear=2023-24&TypeGrouping=offensive
-    private static final String URL = "jdbc:mysql://192.168.56.103:3306/nba"; // Ajusta el puerto si es necesario y el nombre de la base de datos
-    private static final String USER = "perepi";  // Reemplaza con tu usuario
-    private static final String PASSWORD = "pastanaga";  // Reemplaza con tu contraseña
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver"; // Esto es el driver de JDBC para MySQL
+//todo://https://stats.nba.com/stats/synergyplaytypes?LeagueID=00&PerMode=PerGame&PlayType=Isolation&PlayerOrTeam=P&SeasonType=Playoffs&SeasonYear=2023-24&TypeGrouping=offensive
+//todo:          //PARA COMPORVAR LA CONN USAR EL MAIN SIN LOS SELECTS
 
-    public static Connection getConnection() {
+public class DatabaseConnection {
+    //MAQUINA BD/ 10.0.2.15
+    //nba
+    //root patata????????????
+     private static final String URL = "jdbc:mysql://localhost:3306/nba";
+    private static final String USER = "root";
+    private static final String PASSWORD = "patata";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    public static Connection getConnection() throws SQLException {
         try {
-            Class.forName(DRIVER); // Asegura que el driver JDBC está cargado.
+            Class.forName(DRIVER);
             return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (ClassNotFoundException | SQLException e) {
-            throw new RuntimeException("Error conectando a la base de datos", e);
-        }
-    }
-
-    public static void main(String[] args) {
-        try (Connection connection = DatabaseConnection.getConnection()) {
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT nom, cognom FROM jugadors"); // Consulta de ejemplo
-            while (resultSet.next()) {
-                String nom = resultSet.getString("nom");
-                String congnoms = resultSet.getString("cognom");
-                System.out.println("Jugador: " + nom + ", cognom: " + congnoms);
-            }
-            resultSet.close(); // Cerrar el ResultSet
-        } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Error connectant a la base de dades", e);
         }
     }
 }
